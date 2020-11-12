3fa5caced26ce8f24de138b0ec19275d9143e83e
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-11-12 17:42:16 +0200

    Exercise 3.6

--- viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -39,8 +39,19 @@ public class AuthenticationService {
     }
 
     private boolean invalid(String username, String password) {
-        // validity check of username and password
+        if (username.length() < 3 || !username.matches("[a-z]+")) {
+            return true;
+        }
 
-        return false;
+        if (password.length() < 8) {
+            return true;
+        }
+
+        for (char c : password.toCharArray()) {
+            if (!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- viikko3/LoginCucumber/src/test/java/ohtu/Stepdefs.java
+++ viikko3/LoginCucumber/src/test/java/ohtu/Stepdefs.java
@@ -30,6 +30,18 @@ public class Stepdefs {
         inputLines.add("login");
     }
 
+    @Given("^command new is selected$")
+    public void commandNewSelected() throws Throwable {
+        inputLines.add("new");
+    }
+
+    @Given("user {string} with password {string} is created")
+    public void commandNewSelected(String username, String password) throws Throwable {
+        inputLines.add("new");
+        inputLines.add(username);
+        inputLines.add(password);
+    }
+
     @When("username {string} and password {string} are entered")
     public void usernameAndPasswordAreEntered(String username, String password) {
        inputLines.add(username);
