909a98490b1682f98c8ff2f8b15043dd83c51af7
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-11-14 17:54:41 +0200

    Exercise 3.7 and 3.8

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Random;
+
 public class Tester {
 
     public static void main(String[] args) {
@@ -13,14 +15,56 @@ public class Tester {
         driver.get("http://localhost:4567");
         
         sleep(2);
-        
-        WebElement element = driver.findElement(By.linkText("login"));
+
+        Random r = new Random();
+        int randInt = r.nextInt(100000);
+
+        // uuden käyttäjätunnuksen luominen
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka"+randInt);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+
+        sleep(2);
+
+        //uuden käyttäjätunnuksen luomisen jälkeen tapahtuva ulkoskirjautuminen sovelluksesta
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        sleep(2);
+
+        // epäonnistunut kirjautuminen: oikea käyttäjätunnus, väärä salasana
+        element = driver.findElement(By.linkText("login"));
         element.click();
 
         sleep(2);
 
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("wrong");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+        sleep(2);
+
+        // onnistunut kirjautuminen
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka"+randInt);
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
