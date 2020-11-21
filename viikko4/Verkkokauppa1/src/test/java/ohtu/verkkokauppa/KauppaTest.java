a3e6d0bde70e2a1c54d76a6b684dfb12b894810f
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-11-22 00:08:36 +0200

    Exercise 4.4

--- viikko4/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -103,8 +103,8 @@ public class KauppaTest {
     @Test
     public void ostoksenPaatyttyaPankinMetodinTilisiirtoParametritOvatOikeinVaikkaToinenTuoteOnLoppu() {
 
-        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
-        when(varasto.saldo(1)).thenReturn(10);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 1
+        when(varasto.saldo(1)).thenReturn(1);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
         // piimä on loppu
         when(varasto.saldo(2)).thenReturn(0);
@@ -118,5 +118,79 @@ public class KauppaTest {
 
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla parametreilla
         verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(5));
+    }
+
+    @Test
+    public void edellisenOstoksenHintaEiVaikutaUudenOstoksenHintaan() {
+
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla parametreilla
+        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(5));
+
+        // tehdään toiset ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("matti", "54321");
+
+        // varmistetaan, että edellisen ostoksen hinta ei vaikuta uuteen ostokseen
+        verify(pankki).tilisiirto(eq("matti"), anyInt(), eq("54321"), anyString(), eq(5));
+    }
+
+    @Test
+    public void jokaiselleOstokselleLuodaanOmaViiteNumero() {
+        when(viite.uusi())
+                .thenReturn(42)
+                .thenReturn(43)
+                .thenReturn(44);
+
+
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikealla viitenumerolla
+        verify(pankki).tilisiirto(anyString(), eq(42), anyString(), anyString(), anyInt());
+
+        // tehdään toiset ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("matti", "5432§");
+
+        // varmistetaan, että toisessa ostoksessa käytetään toista viitenumeroa
+        verify(pankki).tilisiirto(anyString(), eq(43), anyString(), anyString(), anyInt());
+    }
+
+    @Test
+    public void tuotteenPoistaminenOstoskoristaVaikuttaaHOstostenHintaan() {
+
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        // määritellään että tuote numero 2 on piimä jonka hinta on 6 ja saldo 10
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "piimä", 6));
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan kaksi kappaletta tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);
+        k.poistaKorista(1);   // poistetaan toinen
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla parametreilla
+        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(6));
     }
 }
