0fbaf44395c77556c879b1e178e74e26681dd2c8
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-12-06 10:29:44 +0200

    Exercise 6.2

---
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -0,0 +1,20 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt";
+
+        Statistics stats = new Statistics(new PlayerReaderImpl(url));
+
+        Matcher m = new And(
+            new Not( new HasAtLeast(1, "goals") ),
+            new PlaysIn("NYR")
+        );
+        
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
+        }
+    }
+}
